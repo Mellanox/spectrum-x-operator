4eb2acc145583036c523a7cf0385901865f4b041
Ivan Kolodiazhnyi <ikolodiazhny@nvidia.com>
2025-03-24 13:20:57 +0200

    feat: SR-IOV Controller implementation

--- cmd/spectrum-x-manager/main.go
+++ cmd/spectrum-x-manager/main.go
@@ -170,6 +170,15 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.SRIOVReconciler{
+		Client:             mgr.GetClient(),
+		ConfigMapNamespace: configMapNamespace,
+		ConfigMapName:      configMapName,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create SRIOVReconciler", "SRIOVReconciler", "ConfigMap")
+		os.Exit(1)
+	}
+
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
